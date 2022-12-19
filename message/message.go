f75945184524a73a7efd990fb0504dbaab1e349b
alvin tan <alvintan95@gmail.com>
2022-12-19 16:45:45 +0800

    Update message

--- message/message.go
+++ message/message.go
@@ -1,7 +1,7 @@
 package message
 
 const (
-	TransactionTypeReplyMsg = "Select the transaction type this amount belongs to: "
+	TransactionTypeReplyMsg = "Select a transaction type"
 	TransactionReplyMsg     = "Select a category"
 	GenericErrReplyMsg      = "Something went wrong :("
 )
