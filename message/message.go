7c7c6718b5d0e316ff2bc52d3d2907f44d874bf5
Alvin <alvintan95@gmail.com>
2023-06-11 23:30:24 +0800

    edit help message

--- message/message.go
+++ message/message.go
@@ -2,9 +2,18 @@ package message
 
 const (
 	HelpMsg = `
-Message directly to start recording an expenses.
-E.g. "5.50 Chicken Rice" (without the quotes) to record an expense of $5.50 with the description "Chicken Rice".
+Message directly with the format "[amount] [description]" to start recording an expenses. 
+Make sure the amount is purely a number or deciaml with no currency code or symbol.
+The desciption can be made up of any characters.
+
+E.g
+✔️ "5.50 Chicken Rice" (without the quotes) to record an expense of $5.50 with the description "Chicken Rice".
+✔️ "100 Ikea Table" (without the quotes) to record an expense of $100 with the description "Ikea Table".
+❌ "Computer 2400" (without the quotes) will give an error".
+❌ "$20.78 Pizza" (without the quotes) will give an error".
 	
+The recorded dollar ($) is the default currency symbol with support to up to 2 decimal places for the cents.
+
 Type /stats [month] [year] to view the breakdown for the month.
 Type /list [month] [year] to view the expenses for the month.
 Type /export [month] [year] to export the expenses for the month.
